2e13197cb1bd21a05380173474b0597a4e917d4d
vasily122 <vasily122@pisem.net>
2015-11-09 05:57:05 +0300

    Create template.py

---
+++ template.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#====================================
+#Prog: 
+#Author: vasily122@pisem.net
+#Date: 2015 nov 7
+# Version: 0.0
+#====================================
+import time,os
