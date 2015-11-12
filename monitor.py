f2cdb975efb4072f71f5c7b7830a75211154e48b
Vasily122 <vasily122@yandex.ru>
2015-11-12 17:58:10 +0300

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#====================================
+#Prog:from cron and if network exists
+#load external module chunk.py, chmod it to a+x and execute
+#monitor.py for monitoring outstanding server\workstation
+#Author: vasily122@pisem.net
+#Date: 2015 nov 12
+#Version: 0.2
+#====================================
+import time,os
+kanal=0
+while kanal==0:
+#	print("+\n")
+#	time.sleep(3)
+	b=os.popen("ping -c1 8.8.8.8").read()
+	if(b[68:77] =="icmp_seq="):
+		kanal=1
+os.system("wget https://raw.githubusercontent.com/vasily122/scripts/master/chunk.py")
+os.system("chmod a+x chunk.py && ./chunk.py")
