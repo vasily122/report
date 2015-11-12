8f2fafb35d76a3ef39fb67f756d184b7a7aabb7c
Vasily122 <vasily122@yandex.ru>
2015-11-12 17:41:01 +0300

    Update test1.py
    code cleaned

--- test1.py
+++ test1.py
@@ -10,23 +10,11 @@
 #====================================
 import time,os
 kanal=0
-#print("===start===")
-
 while kanal==0:
 #	print("+\n")
 #	time.sleep(3)
 	b=os.popen("ping -c1 8.8.8.8").read()
 	if(b[68:77] =="icmp_seq="):
 		kanal=1
-
-
 os.system("wget https://raw.githubusercontent.com/vasily122/scripts/master/chunk.py")
 os.system("chmod a+x chunk.py && ./chunk.py")
-
-#os.system("rm chunk.py")
-#os.system("./chunk.py >> out.txt")
-#os.system("cp /home/arina122/DO/chunk.py .")
-#os.system("chmod a+x chunk.py && ./chunk.py")
-#os.system("pwd>>position.txt")
-
-#print("""(uname -a && uptime -p && df) | mail -s "new report" vasily122@yandex.ru""")
