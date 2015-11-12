5fa4ec00b77822c63ecf6f2e67b742da7687b704
Vasily122 <vasily122@yandex.ru>
2015-11-12 17:34:21 +0300

    Update report.py

--- report.py
+++ report.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
+#====================================
+#Prog:from cron and if network exists
+#run report.py to monitoring outstanding server\workstation
+#chmod a+x report.py
+#crontab -e : @reboot /home/arina122/DO/./report.py
+#Author: vasily122@yandex.ru
+#Date: 2015 nov 12
+#Version: 0.2
+#====================================
 import time,os
 kanal=0
 while kanal==0:
