07c1e7d8daa0e253333851fd7554f3a472416e2f
Vasily122 <vasily122@yandex.ru>
2015-11-13 20:21:03 +0300

    Update template.py

--- template.py
+++ template.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- 
 #====================================
 #Prog: 
-#Author: vasily122@pisem.net
+#Author: vasily122@yandex.ru
 #Date: 2015 nov 7
 # Version: 0.0
 #====================================
