71c287b5e4fb8054486aeccc3c2921bb4636c18f
Vasily122 <vasily122@yandex.ru>
2015-11-13 20:16:18 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+point=sort_$(date +%d_%m_%Y)
+#touch ~/Рабочий\ стол/cleaned$(date +%d_%m_%Y).txt
+#DISPLAY=:0 gdialog --msgbox "SDELAY PERERYV \!" 25 20 > /dev/null
+#rm -R /home/evrosvet/.thumbnails
+#rm /home/evrosvet/Рабочий\ стол/cleaned*.txt
+# ./test.sh >> ~/Рабочий\ стол/1.txt
+#mkdir ~/to_sort/$point
+#mv ~/Загрузки/*.* ~/to_sort/sort_$(date +%d_%m_%Y)
+
+
+
+echo $point
