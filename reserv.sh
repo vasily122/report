f5c97d143728518ead5da6439b4cdd5d21163327
Vasily122 <vasily122@yandex.ru>
2015-11-13 20:14:05 +0300

    Create reserv.sh

---
+++ reserv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /mnt/backup
+sudo tar cvpzf /home/evrosvet/backup_`date +%Y.%m.%d_%H_%M`.tgz --exclude=/media --exclude=/proc --exclude=/lost+found --exclude=/mnt  --exclude=/home --exclude=/sys / >> /dev/null 2>&1
