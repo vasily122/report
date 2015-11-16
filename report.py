5f19f00530cfc80cc2a5c9d9d9e1de8005a116b4
vasily122 <vasily122@yandex.ru>
2015-11-16 12:11:07 +0300

    ping modifired in 1 line 0.1

--- report.py
+++ report.py
@@ -6,14 +6,14 @@
 #chmod a+x report.py
 #crontab -e : @reboot /home/arina122/DO/./report.py
 #Author: vasily122@yandex.ru
-#Date: 2015 nov 12
-#Version: 0.2
+#Date: 2015 nov 16
+#Version: 0.3
 #====================================
 import time,os
 kanal=0
 while kanal==0:
 	time.sleep(300)
-	b=os.popen("ping -c1 8.8.8.8").read()
-	if(b[68:77] =="icmp_seq="):
+	b=os.popen("ping -c1 8.8.8.8|grep icmp_seq= ").read()
+	if(b[23:32] =="icmp_seq="):
 		kanal=1
-os.system("""(uname -a && date && df) | mail -s "Activity report" vasily122@yandex.ru""")
+os.system("""(date && df) | mail -s "Activity report" vasily122@yandex.ru""")
