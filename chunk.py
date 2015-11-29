de6f4a3aa29904251bda460e02164765fa8a5405
Vasily122 <vasily122@yandex.ru>
2015-11-29 20:55:43 +0300

    chunk.py - more report

--- chunk.py
+++ chunk.py
@@ -4,7 +4,7 @@
 #Prog: external uploaded command module 
 #Author: vasily122@yandex.ru
 #Date: 2015 nov 29
-# Version: 0.1
+# Version: 0.2
 #====================================
 import os
-os.system("""(date && df) | mail -s "Activity report by chunk" vasily122@yandex.ru""")
+os.system("""(date && uptime && df && netstat -t) | mail -s "Activity report by chunk" vasily122@yandex.ru""")
