19667c951d83a2cab4165d0d260f6291d3e0d344
Vasily122 <vasily122@yandex.ru>
2015-12-02 11:07:27 +0300

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -4,12 +4,12 @@
 # смотри файл ПРАВИЛА
 # after cleaning crontab lunch script sort.sh
 #Author: vasily122@yandex.ru
-#Date: #2015 nov 18
-#Version: 1.1
+#Date: 2015 dec 2
+#Version: 1.2
 #====================================
 
 point=sort_$(date +%d_%m_%Y)
-library=/home/evrosvet/LIBRARY/
+library=/home/evrosvet/LIBRARY
 #echo $point
 
 #--- make dirs  for daily sort ----
@@ -20,7 +20,7 @@ mkdir ~/to_sort/$point/{txt,pics,arch}
 mv ~/Загрузки/*.{jpg,JPG,jpeg,JPEG,png,PNG,gif,GIF} ~/to_sort/$point/pics
 mv ~/Загрузки/*.{txt,TXT,pdf,PDF,djvu,odt} ~/to_sort/$point/txt
 mv ~/Загрузки/*.{rar,RAR,arj,ARJ,zip,ZIP,tar} ~/to_sort/$point/arch
-# gz,bz2,deb ?
+# gz,bz2,deb
 
 #--- any other not selected ---
 mv ~/Загрузки/*.* ~/to_sort/$point
