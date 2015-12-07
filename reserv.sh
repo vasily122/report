6802f76a561812ca17c17dfee9ab0959b6a32341
Vasily122 <vasily122@yandex.ru>
2015-12-07 15:07:55 +0300

    Delete reserv.sh

--- reserv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# prototipe only, carefull!
-cd /mnt/backup
-sudo tar cvpzf /home/evrosvet/backup_`date +%Y.%m.%d_%H_%M`.tgz --exclude=/media --exclude=/proc --exclude=/lost+found --exclude=/mnt  --exclude=/home --exclude=/sys / >> /dev/null 2>&1
