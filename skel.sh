00ae1851fdfe152fd9532f0138abb5d96d09965a
Vasily122 <vasily122@yandex.ru>
2015-12-17 16:30:00 +0300

    correct directories

--- skel.sh
+++ skel.sh
@@ -2,11 +2,12 @@
 # Prog: create empty folders in LIBRARY
 # for propper work of clean.sh and sort.sh
 # Author: vasily122@yandex.ru
-# Date: 2015 dec 7
-# Version: 0.1
+# Date: 2015 dec 17
+# Version: 0.2
 #====================================
 # mkdir ~/LIBRARY
-mkdir ~/LIBRARY/{7v1,arch,avatars,book,draw,girl,howto,info,linux,micro,phys,pics,prog,search,stoks,txt,workplaces}
+#mkdir ~/LIBRARY/{7v1,arch,avatars,book,draw,girl,howto,info,linux,micro,phys,pics,prog,search,stoks,txt,workplaces}
+mkdir ~/LIBRARY/{7v,arch,book,draw,howto,info,linux,micro,phys,pics,prog,search,stoks,txt,workplaces}
 
 #for i in {7v1,arch,avatars,book,draw,girl,howto,info,linux,micro,phys,pics,prog,search,stoks,txt,workplaces}
 #do  mkdir $i ;
