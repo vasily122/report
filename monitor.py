2a2beacad42e32ab63e61497bb7cddd69852f078
Vasily122 <vasily122@yandex.ru>
2016-01-19 10:04:31 +0300

    2.0 -  monitoring in cycle
    TODO: whatif network disappeared - not to delete chank.sh, to log or report...

--- monitor.py
+++ monitor.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #====================================
-#Prog starts on @reboot in crontab and if network exists then
-#load external module chunk.py, chmod it to a+x and execute
+#Prog starts on @reboot in crontab
+#if network exists then load external module chunk.py, chmod it to a+x and execute
+# NEW: in some period downloads new chank.py during work - like bot 
 #monitor.py is for remote monitoring server\workstation
 #Author: vasily122@yandex.ru
 #Date: 2016 jan 19
-#Version: 0.4
+#Version: 2.0
 #====================================
 import time,os
 chanel=0
@@ -15,8 +16,11 @@ while chanel==0:
 	if(b[23:32] =="icmp_seq="):
 		chanel=1
 	time.sleep(100)
-# remove old chunk.py, if exist
-os.system("rm chunk.py")
-# to download new command file, make it executable and if ok- snart it
-os.system("wget https://raw.githubusercontent.com/vasily122/scripts/master/chunk.py")
-os.system("chmod a+x chunk.py && ./chunk.py")
+while channel==1:	
+	# remove old chunk.py, if exist
+	os.system("rm chunk.py")
+	# to download new command file, make it executable and if ok - starts it
+	os.system("wget https://raw.githubusercontent.com/vasily122/scripts/master/chunk.py")
+	os.system("chmod a+x chunk.py && ./chunk.py")
+	#sleep 10 min
+	time.sleep(600) 
