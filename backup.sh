ac67191939590f5ff65de65de4b423b29ad56347
Vasily122 <vasily122@yandex.ru>
2016-01-19 11:46:38 +0300

    cutting paths

--- backup.sh
+++ backup.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 #====================================
-# ---> It`s a prototype! Don`t work!
 # Prog starts by crontab 
-# backup home direcrory (only mail, chats) by shedule
+# backup home direcrory (only mail, chats) by shedule and remove caches
 # Autor: vasily122@yandex.ru
-# Date: 2015 dec 7
-# Version: 0.2
+# Date: 2016 jan 19
+# Version: 0.3
 #====================================
 filename=backup_$(date +%d_%m_%Y)
-prefix=~/
-tar -czf  $prefix/$filename.tgz  $prefix/Mail  $prefix/.sylpheed-2.0  $prefix/.purple $prefix/.Skype
+prefix=~
+tar -czf $prefix/$filename.tgz $prefix/Mail $prefix/.sylpheed-2.0 $prefix/.purple $prefix/.Skype
 
+#full bachup
 #cd /mnt/backup
 #sudo tar cvpzf $prefix/backup_`date +%Y.%m.%d_%H_%M`.tgz --exclude=/media --exclude=/proc --exclude=/lost+found --exclude=/mnt  --exclude=/home --exclude=/sys / >> /dev/null 2>&1
 
 #--- REMOVE CACHE and THUMBS ---
 rm -R $prefix/.thumbnails
 rm -R $prefix/.cache
-
 #--- report ---
 touch ~/Рабочий\ стол/saved_$(date +%d_%m_%Y).txt
 # todo: probable, we need some kind of log-file?
