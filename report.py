1be5326ef99f97e8043ae52473970789aabff065
Vasily122 <vasily122@yandex.ru>
2016-01-19 09:38:15 +0300

    changed sleep order and time

--- report.py
+++ report.py
@@ -7,16 +7,16 @@
 #note: chmod a+x report.py
 #crontab -e : @reboot ~/DO/./report.py
 #Author: vasily122@yandex.ru
-#Date: 2015 dec 8
-#Version: 1.3
+#Date: 2016 jan 19
+#Version: 1.4
 #====================================
 import time,os
 chanel=0
 while chanel==0:
-	time.sleep(300)  #every 5 minutes until network appear 
 	b=os.popen("ping -c1 8.8.8.8|grep icmp_seq= ").read()
 	if(b[23:32] =="icmp_seq="):
 		chanel=1
+	time.sleep(100)  #wait 100 sec until network appear	
 os.system("""(date && df) | mail -s "Activity report" vasily122@yandex.ru""")
 
 #DONE next idea is to split report.py to: monitor.py (resident) and chunk.py (download every time,changeble)
