c5a1096ea9456f2e1da28b9fd3446d5d90bb556d
Vasily122 <vasily122@yandex.ru>
2016-01-19 09:31:02 +0300

    polish file

--- skel.sh
+++ skel.sh
@@ -2,13 +2,12 @@
 # Prog: create empty folders in LIBRARY
 # for propper work of clean.sh and sort.sh
 # Author: vasily122@yandex.ru
-# Date: 2015 dec 17
-# Version: 0.2
+# Date: 2016 jan 19
+# Version: 0.3
 #====================================
-# mkdir ~/LIBRARY
-#mkdir ~/LIBRARY/{7v1,arch,avatars,book,draw,girl,howto,info,linux,micro,phys,pics,prog,search,stoks,txt,workplaces}
+#v1
 mkdir ~/LIBRARY/{7v,arch,book,draw,howto,info,linux,micro,phys,pics,prog,search,stoks,txt,workplaces}
-
-#for i in {7v1,arch,avatars,book,draw,girl,howto,info,linux,micro,phys,pics,prog,search,stoks,txt,workplaces}
+#v2 
+#for i in {7v,arch,avatars,book,draw,howto,info,linux,micro,phys,pics,prog,search,stoks,txt,workplaces}
 #do  mkdir $i ;
 #done
