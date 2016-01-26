88a312df5d935093b0be80ba1c8760021ee6d919
Vasily122 <vasily122@yandex.ru>
2016-01-26 18:03:41 +0300

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -2,9 +2,9 @@
 #====================================
 #Prog starts in time by crontab and cleans download folder "~/Загрузки"
 #look ПРАВИЛА
-#after cleaning crontab lunch script sort.sh
+#after clean.sh crontab lunchs sort.sh
 #Author: vasily122@yandex.ru
-#Date: 2016 jan 19
+#Date: 2016 jan 26
 #Version: 1.5
 #====================================
 point=sort_$(date +%d_%m_%Y)
@@ -15,8 +15,8 @@ mkdir ~/to_sort/$point/{txt,pics,arch}
 #--- sort files by type in daily sort dir ---
 mv ~/Загрузки/*.{jpg,JPG,jpeg,JPEG,png,PNG,gif,GIF} ~/to_sort/$point/pics
 mv ~/Загрузки/*.{txt,TXT,pdf,PDF,djvu,odt} ~/to_sort/$point/txt
-mv ~/Загрузки/*.{rar,RAR,arj,ARJ,zip,ZIP,tar,7z} ~/to_sort/$point/arch
-# gz,bz2,deb,tgz
+mv ~/Загрузки/*.{rar,RAR,arj,ARJ,zip,ZIP,tar,7z,gz,bz2,tgz} ~/to_sort/$point/arch
+# deb
 #--- any other not selected ---
 mv ~/Загрузки/*.* ~/to_sort/$point
 #--- report ---
