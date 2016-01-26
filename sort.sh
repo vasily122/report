fca8b49cf42d47705792c92ae6b42cf42b07aca4
Vasily122 <vasily122@yandex.ru>
2016-01-26 18:00:30 +0300

    Update sort.sh

--- sort.sh
+++ sort.sh
@@ -3,39 +3,23 @@
 #Prog starts by crontab AFTER clean.sh
 #sort and move files by TAGS to folder ~/LIBRARY
 #Autor: vasily122@yandex.ru
-#Date: 2016 jan 19
-#Version: 2.3.1
+#Date: 2016 jan 26
+#Version: 2.4
 #====================================
-
 point=sort_$(date +%d_%m_%Y)
 library=~/LIBRARY
 #ПРАВИЛА
-#howto   полезные инструкции и руководства
-#stoks   относится к акциям, рынку, деньгам вцелом
-#book    книги по всем темам - если надо- вводить подразделы
-#search  ссылки и темы для дальнейшего поиска
-#info    информационные документы, справочники
-#micro   микропроцессоры, контроллеры, ARDUINO,PINGUINO,PIC etc.
-#prog    программирование, языки, ноу-хау
-#phys    планетология, физика, другие науки
-#girl    наборы картинок
-#linux   администрирование всех UNIX\Linux\BSD
-
 # Сначала архивы приравняем к текстам и отправим на сортировку по ТЭГАМ
-mv -S.bak -v ~/to_sort/$point/arch/*.* ~/to_sort/$point/txt
-
-#-- MV files to permanent /home/evrosvet/LIBRARY/
 #-- suffix .bak, -verbose, -update by newer
-for i in {howto,stoks,book,search,info,prog,phys,micro,linux} :
-   do mv -S.bak -v ~/to_sort/$point/txt/$i* $library/$i/
+mv -S.bak -v ~/to_sort/$point/arch/*.* ~/to_sort/$point/txt
+# -- {..} taken from skel.sh
+for i in {arch,book,draw,howto,info,linux,micro,phys,prog,search,stoks,radio,hack} :
+   do mv -S.bak -v ~/to_sort/$point/txt/$i* $library/$i/    
 done
 # остатки от сортировки- в общую папку
 mv -S.bak -v ~/to_sort/$point/txt/*.*  $library/
 mv -S.bak -v ~/to_sort/$point/pics/*.* $library/pics
-
 #некатегорированные архивы- в общую папку
 mv -S.bak -v ~/to_sort/$point/*.* $library/
-
 #--- report ---
 echo -e "sorted_$(date +%d_%m_%Y) \n" >> ~/to_sort/log.txt
-
