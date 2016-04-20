cc25fe2678be8107bed0b48ed546f85952f3f88a
Vasily122 <vasily122@yandex.ru>
2016-04-20 11:16:56 +0300

    change path to log.txt

--- bkplib.sh
+++ bkplib.sh
@@ -3,13 +3,13 @@
 # Prog starts by crontab
 # incremental backup of library: files, added last month
 # Autor: vasily122@yandex.ru
-# Date: 2016 mar 11
-# Version: 1.1
+# Date: 2016 apr 20
+# Version: 1.2
 #====================================
 filename=LIBRARY_$(date +%d_%m_%Y)
 prefix=~
-tar cvzf - `find ~/LIBRARY -ctime +30 -print` > $prefix/$filename.tgz
+tar cvzf - `find ~/LIBRARY -ctime +11 -print` > $prefix/$filename.tgz
 #touch ~/Рабочий\ стол/saved_$(date +%d_%m_%Y).txt
 #report to logfile and mail
-echo -e "BACKUP_LIBRARY_$(date +%d_%m_%Y)" >> ~/to_sort/log.txt
+echo -e "BACKUP_LIBRARY_$(date +%d_%m_%Y)" >> ~/log.txt
 echo -e "Backup_LIBRARY_$(date +%d_%m_%Y)" | mail -s "Backup LIB report" vasily122@yandex.ru
