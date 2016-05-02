06f719df14af4043f2ca33eef918492719a648f7
Vasily122 <vasily122@yandex.ru>
2016-05-02 10:27:43 +0400

    tail boinc 0

--- chunk.py
+++ chunk.py
@@ -7,4 +7,6 @@
 # Version: 0.3
 #====================================
 import os
-os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "Activity report" vasily122@yandex.ru""")
+#os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "Activity report" vasily122@yandex.ru""")
+
+os.system("""(date ; tail /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; tail /var/lib/boinc-client/time_stats_log)| mail -s "Activity report" vasily122@yandex.ru""")
