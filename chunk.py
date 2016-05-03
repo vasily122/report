3e45bd7eea53a2318d708dc30765c73b5b03be93
Vasily122 <vasily122@yandex.ru>
2016-05-03 11:48:56 +0400

    uptime

--- chunk.py
+++ chunk.py
@@ -10,4 +10,6 @@ import os
 #os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "Activity report" vasily122@yandex.ru""")
 #os.system("""(date ; tail /var/lib/boinc-client/stderrdae.txt; tail /var/lib/boinc-client/stdoutdae.txt; tail /var/lib/boinc-client/time_stats_log)| mail -s "Activity report" vasily122@yandex.ru""")
 
-os.system("""(date ; tail /var/lib/boinc-client/stderrdae.txt; report-hw)| mail -s "system data" vasily122@yandex.ru""")
+#os.system("""(date ; tail /var/lib/boinc-client/stderrdae.txt; report-hw)| mail -s "system data" vasily122@yandex.ru""")
+
+os.system("""(date; uptime)| mail -s "system data" vasily122@yandex.ru""")
