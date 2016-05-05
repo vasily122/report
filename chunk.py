998254770c992ae6a4b104faf07b3e8f6ecf269e
Vasily122 <vasily122@yandex.ru>
2016-05-05 12:11:52 +0300

    Update chunk.py

--- chunk.py
+++ chunk.py
@@ -9,7 +9,6 @@
 import os
 #os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "Activity report" vasily122@yandex.ru""")
 #os.system("""(date ; tail /var/lib/boinc-client/stderrdae.txt; tail /var/lib/boinc-client/stdoutdae.txt; tail /var/lib/boinc-client/time_stats_log)| mail -s "Activity report" vasily122@yandex.ru""")
-
 #os.system("""(date ; tail /var/lib/boinc-client/stderrdae.txt; report-hw)| mail -s "system data" vasily122@yandex.ru""")
-
-os.system("""(date; uptime)>>log.txt""")
+#os.system("""(date; uptime)>>log.txt""")
+os.system("""sudo killall boinc; ps ax | mail -s "PS boinc" vasily122@yandex.ru""")
