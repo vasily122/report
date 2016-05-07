8b23fd7601877103818fcf7208bff8a0a677fcc5
Vasily122 <vasily122@yandex.ru>
2016-05-07 18:33:38 +0300

    no internet -off
    Временно отменена проверка на наличие интернет, предполагается что или будет загрузка  или не будет выполнения

--- monitor.py
+++ monitor.py
@@ -6,21 +6,12 @@
 # NEW: in some period downloads new chank.py during work - like bot 
 #monitor.py is for remote monitoring server\workstation
 #Author: vasily122@yandex.ru
-#Date: 2016 jan 19
-#Version: 2.0
+#Date: 2016 may 07
+#Version: 2.0.1
 #====================================
-import time,os
-chanel=0
-while chanel==0:
-	b=os.popen("ping 8.8.8.8 -c1 |grep icmp_seq=").read()
-	if(b[23:32] =="icmp_seq="):
-		chanel=1
-	time.sleep(100)
-while channel==1:	
-	# remove old chunk.py, if exist
-	os.system("rm chunk.py")
-	# to download new command file, make it executable and if ok - starts it
-	os.system("wget https://raw.githubusercontent.com/vasily122/scripts/master/chunk.py")
-	os.system("chmod a+x chunk.py && ./chunk.py")
-	#sleep 10 min
-	time.sleep(600) 
+import os
+#remove old chunk.py, if exist
+os.system("rm s50_task.py")
+# to download new command file, make it executable and if ok - starts it
+os.system("wget https://raw.githubusercontent.com/vasily122/scripts/master/s50_task.py")
+os.system("chmod a+x s50_task.py && ./s5_task.py")
