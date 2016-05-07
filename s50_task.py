ed47b6a4a266172421c43124cc52278297e4104f
Vasily122 <vasily122@yandex.ru>
2016-05-07 18:28:11 +0300

    Create s50_task.py

---
+++ s50_task.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#====================================
+# Prog: external uploaded command module 
+# Author: vasily122@yandex.ru
+# Date: 2016 may 7
+# Version: 0.3
+#====================================
+import os
+os.system("""(date;uptime;df) | mail -s "s50 task" vasily122@yandex.ru""")
