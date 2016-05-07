efffcfedb70c1f8cb4cca23b7f3f514293eb6e10
Vasily122 <vasily122@yandex.ru>
2016-05-07 11:17:57 +0400

    Update pen4_task.py

--- pen4_task.py
+++ pen4_task.py
@@ -7,8 +7,4 @@
 # Version: 0.3
 #====================================
 import os
-#os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "Activity report" vasily122@yandex.ru""")
-#os.system("""(date ; tail /var/lib/boinc-client/stderrdae.txt; tail /var/lib/boinc-client/stdoutdae.txt; tail /var/lib/boinc-client/time_stats_log)| mail -s "Activity report" vasily122@yandex.ru""")
-#os.system("""(date ; tail /var/lib/boinc-client/stderrdae.txt; report-hw)| mail -s "system data" vasily122@yandex.ru""")
-#os.system("""(date; uptime)>>log.txt""")
-os.system("""date;uptime;df" | mail -s "Pen4 task" vasily122@yandex.ru""")
+os.system("""(date;uptime;df) | mail -s "Pen4 task" vasily122@yandex.ru""")
