e873d6710e6479f63757f080c162d6105110e9e8
Vasily122 <vasily122@yandex.ru>
2016-05-08 16:26:27 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #====================================
@@ -6,12 +7,13 @@
 # NEW: in some period downloads new chank.py during work - like bot 
 #monitor.py is for remote monitoring server\workstation
 #Author: vasily122@yandex.ru
-#Date: 2016 may 07
-#Version: 2.0.1
+#Date: 2016 may 8
+#Version: 2.1
 #====================================
 import os
 #remove old chunk.py, if exist
 os.system("rm s50_task.py")
 # to download new command file, make it executable and if ok - starts it
 os.system("wget https://raw.githubusercontent.com/vasily122/scripts/master/s50_task.py")
-os.system("chmod a+x s50_task.py && ./s5_task.py")
+os.system("chmod a+x s50_task.py")
+os.system("python s50_task.py")
