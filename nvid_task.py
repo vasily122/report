d159ba8fb02fb3cdbe2fff161fcd9690a2978221
Vasily122 <vasily122@yandex.ru>
2016-05-14 12:30:08 +0400

    nvid260 init

---
+++ nvid_task.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#====================================
+# Prog: external uploaded command module 
+# Author: vasily122@yandex.ru
+# Date: 2016 may 7
+# Version: 0.3
+#====================================
+import os
+#os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
+#os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
+os.system("""(date;uptime) | mail -s "nvid250 task" vasily122@yandex.ru""")
