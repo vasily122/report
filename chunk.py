c53574d6fa60179a28a6b40701b5f1b6908c497a
Vasily122 <vasily122@yandex.ru>
2016-05-16 20:54:33 +0400

    Update chunk.py

--- chunk.py
+++ chunk.py
@@ -7,8 +7,8 @@
 # Version: 0.3
 #====================================
 import os
-#os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "Activity report" vasily122@yandex.ru""")
+os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "Activity report" vasily122@yandex.ru""")
 #os.system("""(date ; tail /var/lib/boinc-client/stderrdae.txt; tail /var/lib/boinc-client/stdoutdae.txt; tail /var/lib/boinc-client/time_stats_log)| mail -s "Activity report" vasily122@yandex.ru""")
 #os.system("""(date ; tail /var/lib/boinc-client/stderrdae.txt; report-hw)| mail -s "system data" vasily122@yandex.ru""")
-#os.system("""(date; uptime)>>log.txt""")
-os.system("""sudo killall boinc >>logs.txt; cat logs.txt | mail -s "PS boinc" vasily122@yandex.ru""")
+os.system("""(date; uptime)>>log.txt""")
+#os.system("""sudo killall boinc >>logs.txt; cat logs.txt | mail -s "PS boinc" vasily122@yandex.ru""")
