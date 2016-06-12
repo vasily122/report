0a7748e7cd7ed77713b15c1b6a35a5caf8189f38
Vasily122 <vasily122@yandex.ru>
2016-06-12 17:18:50 +0300

    Update task_pen4.py

--- task_pen4.py
+++ task_pen4.py
@@ -8,4 +8,4 @@
 #====================================
 import os
 #os.system("""(sync;sync;sync;date;ps ax) | mail -s "Pen4 task" vasily122@yandex.ru""")
-os.system("""(sync;sync;sync;dmesg) | mail -s "Pen4 task" vasily122@yandex.ru""")
+os.system("""(sync;date;ps ax) | mail -s "Pen4 task" vasily122@yandex.ru""")
