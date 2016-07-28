425c83597b66d71bb8524b3163ccfbadbc8e8d8a
Vasily122 <vasily122@yandex.ru>
2016-07-28 18:54:44 +0300

    Update task_besl.py

--- task_besl.py
+++ task_besl.py
@@ -3,8 +3,8 @@
 #====================================
 # Prog: external uploaded command module 
 # Author: vasily122@yandex.ru
-# Date: 2016 may 7
+# Date: 2016 july 28
 # Version: 0.3
 #====================================
 import os
-#os.system(""" (date; uptime; ps ax ) | mail -s "s50 task" vasily122@yandex.ru """)
+os.system(""" (date; uptime; ps ax ) | mail -s "samsung r530 task" vasily122@yandex.ru """)
