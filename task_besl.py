6c2fe67d775aa6aee970f53966d92d1b54eb7de2
Vasily122 <vasily122@yandex.ru>
2016-07-28 18:47:08 +0300

    create
    v0

---
+++ task_besl.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#====================================
+# Prog: external uploaded command module 
+# Author: vasily122@yandex.ru
+# Date: 2016 may 7
+# Version: 0.3
+#====================================
+import os
+#os.system(""" (date; uptime; ps ax ) | mail -s "s50 task" vasily122@yandex.ru """)
