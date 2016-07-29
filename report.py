26aa1f1a6229874321269ff60c1731e51c745559
Vasily122 <vasily122@yandex.ru>
2016-07-29 10:29:05 +0100

    New algorithm version 3 -
    Samsung NP-R530-JA06RU
    LUBUNTU 12.04 upgraded 2016 july 28
    In this version ping returns "icmp_req=", in later versions ping returns "icmp_seq="

--- report.py
+++ report.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #====================================
-#Prog:from cron and if network exists
-#run report.py to remote monitoring server\workstation
-#report.py converted to monitor.py+sort.sh
-#note: chmod a+x report.py
-#crontab -e : @reboot ~/DO/./report.py
+#Prog starts on @reboot in crontab
+#if network exists then load external module monitor.py
 #Author: vasily122@yandex.ru
-#Date: 2016 jan 19
-#Version: 1.4
+#Date: 2016 july 28
+#Version: 3.1
 #====================================
 import time,os
-chanel=0
-while chanel==0:
-	b=os.popen("ping -c1 8.8.8.8|grep icmp_seq= ").read()
-	if(b[23:32] =="icmp_seq="):
-		chanel=1
-	time.sleep(100)  #wait 100 sec until network appear	
-os.system("""(date && df) | mail -s "Activity report" vasily122@yandex.ru""")
-
-#DONE next idea is to split report.py to: monitor.py (resident) and chunk.py (download every time,changeble)
+kanal=0
+while kanal==0:
+	b=os.popen("ping -c1 8.8.8.8").read()
+	if(b[68:77] == "icmp_req=" ):
+#	b=os.popen("ping -c1 8.8.8.8|grep icmp_seq= ").read()
+#	if(b[23:32] =="icmp_seq="):	
+		kanal=1
+		break
+	else:
+		time.sleep(300)
+os.system("""python /home/toma/DO/./monitor.py""")
