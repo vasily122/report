a053ee1d3e31c63e4b04f8ee7309b6251450fad6
Vasily122 <vasily122@yandex.ru>
2016-09-05 15:51:00 +0300

    NOP

--- task_s50.py
+++ task_s50.py
@@ -7,6 +7,6 @@
 # Date: 2016 may 7
 # Version: 0.3
 #====================================
-import os
+#import os
 #os.system(""" (date; uptime; ps ax ) | mail -s "s50 task" vasily122@yandex.ru """)
 #os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc s50" vasily122@yandex.ru""")
