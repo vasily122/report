f9457ec92640a15963d290942280ddd7b94abfaa
Vasily122 <vasily122@yandex.ru>
2016-09-05 15:45:25 +0300

    NOP

--- task_nvid.py
+++ task_nvid.py
@@ -6,7 +6,7 @@
 # Date: 2016 may 7
 # Version: 0.3
 #====================================
-import os
+#import os
 #os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc nvid" vasily122@yandex.ru""")
