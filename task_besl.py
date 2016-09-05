a92e065f5092d3019f13720a11d54fec3ea9fe63
Vasily122 <vasily122@yandex.ru>
2016-09-05 15:46:57 +0300

    NOP

--- task_besl.py
+++ task_besl.py
@@ -6,6 +6,6 @@
 # Date: 2016 july 28
 # Version: 0.4
 #====================================
-import os
-os.system("""sync; date >> ~/DO/log.txt """)
-os.system("""(date; ifconfig; df) | mail -s "samsung r530 task" vasily122@yandex.ru""")
+#import os
+#os.system("""sync; date >> ~/DO/log.txt """)
+#os.system("""(date; ifconfig; df) | mail -s "samsung r530 task" vasily122@yandex.ru""")
