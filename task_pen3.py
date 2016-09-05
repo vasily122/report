971cb431f7633628f0ae512fc8bb972381fa7541
Vasily122 <vasily122@yandex.ru>
2016-09-05 15:48:36 +0300

    NOP

--- task_pen3.py
+++ task_pen3.py
@@ -6,7 +6,7 @@
 # Date: 2016 may 7
 # Version: 0.3
 #====================================
-import os
+#import os
 #os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;ps ax) | mail -s "pen3 task" vasily122@yandex.ru""")
