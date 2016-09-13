e3c290cd2e13da97b777e37550ae340928c5c638
Vasily122 <vasily122@yandex.ru>
2016-09-13 08:56:04 +0100

    look up process

--- task_s5000.py
+++ task_s5000.py
@@ -6,8 +6,8 @@
 # Date: 2016 may 7
 # Version: 0.3
 #====================================
-#import os
+import os
 #os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;uptime) | mail -s "s5000 task" vasily122@yandex.ru""")
-#os.system("""(date; uptime; ps ax ) | mail -s "s5000 task" vasily122@yandex.ru""")
+os.system("""(date; uptime; ps ax |grep boinc; ps ax |grep rosetta ) | mail -s "s5000 task" vasily122@yandex.ru""")
