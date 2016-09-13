b2c660a8acbe0cceac4611e2053401fc6141ddd3
Vasily122 <vasily122@yandex.ru>
2016-09-13 14:04:09 +0100

    NOP

--- task_s5000.py
+++ task_s5000.py
@@ -6,14 +6,14 @@
 # Date: 2016 may 7
 # Version: 0.3
 #====================================
-import os
+#import os
 #os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;uptime) | mail -s "s5000 task" vasily122@yandex.ru""")
 
 #os.system("""(date;  ls -la /var/lib/boinc-client/) | mail -s "s5000 ls /var/lib/boinc-client/" vasily122@yandex.ru""")
-os.system("""tar -cvf b2.tar /var/lib/boinc-client/ ;  (date; uptime) | mutt -s " S5000 boinc dir" vasily122@yandex.ru -a b2.tar""")
-os.system(""" rm b2.tar""")
+#os.system("""tar -cvf b2.tar /var/lib/boinc-client/ ;  (date; uptime) | mutt -s " S5000 boinc dir" vasily122@yandex.ru -a b2.tar""")
+#os.system(""" rm b2.tar""")
 
 #os.system("""(date;  ls -la /var/lib/boinc-client/projects) | mail -s "s5000 ls /var/lib/boinc-client/projects" vasily122@yandex.ru""")
 #os.system("""(date;  ls -la /var/lib/boinc-client/slots) | mail -s "s5000 ls /var/lib/boinc-client/slots" vasily122@yandex.ru""")
