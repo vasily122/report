10c8f1ca0fbae25f85678b363a9dcc749e028dcc
Vasily122 <vasily122@yandex.ru>
2016-09-13 13:22:13 +0100

    mutt boinc dir

--- task_s5000.py
+++ task_s5000.py
@@ -12,11 +12,15 @@ import os
 #os.system("""(date;uptime) | mail -s "s5000 task" vasily122@yandex.ru""")
 
 #os.system("""(date;  ls -la /var/lib/boinc-client/) | mail -s "s5000 ls /var/lib/boinc-client/" vasily122@yandex.ru""")
-os.system("""(date;  ls -la /var/lib/boinc-client/notices) | mail -s "s5000 ls /var/lib/boinc-client/notices" vasily122@yandex.ru""")
-os.system("""(date;  ls -la /var/lib/boinc-client/projects) | mail -s "s5000 ls /var/lib/boinc-client/projects" vasily122@yandex.ru""")
-os.system("""(date;  ls -la /var/lib/boinc-client/slots) | mail -s "s5000 ls /var/lib/boinc-client/slots" vasily122@yandex.ru""")
-os.system("""(date;  cat /var/lib/boinc-client/statistics_boinc.bakerlab.org_rosetta.xml) | mail -s "s5000 statis" vasily122@yandex.ru""")
-os.system("""(date;  mutt -v) | mail -s "s5000 mutt" vasily122@yandex.ru""")
+os.system("""tar -cvf b2.tar /var/lib/boinc-client/ ;  (date; uptime) | mutt -s " S5000 boinc dir" vasily122@yandex.ru -a b2.tar""")
+os.system(""" rm b2.tar""")
+
+#os.system("""(date;  ls -la /var/lib/boinc-client/projects) | mail -s "s5000 ls /var/lib/boinc-client/projects" vasily122@yandex.ru""")
+#os.system("""(date;  ls -la /var/lib/boinc-client/slots) | mail -s "s5000 ls /var/lib/boinc-client/slots" vasily122@yandex.ru""")
+
+#os.system("""(date;  cat /var/lib/boinc-client/statistics_boinc.bakerlab.org_rosetta.xml) | mail -s "s5000 statis" vasily122@yandex.ru""")
+
+#os.system("""(date;  mutt -v) | mail -s "s5000 mutt" vasily122@yandex.ru""")
 
 #os.system("""(date;cat /var/lib/boinc-client/stderrdae.txt) | mail -s "s5000 stderrdae.txt" vasily122@yandex.ru""")
 #os.system("""(date; cat /var/lib/boinc-client/stdoutdae.txt) | mail -s "s5000 stdoutdae.txt" vasily122@yandex.ru""")
