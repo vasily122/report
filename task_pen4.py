ec7eabb3b618593af86e3e2eb5587e788c598d54
Vasily122 <vasily122@yandex.ru>
2016-10-20 15:37:39 +0100

    Update task_pen4.py

--- task_pen4.py
+++ task_pen4.py
@@ -6,6 +6,6 @@
 # Date: 2016 may 7
 # Version: 0.3
 #====================================
-#import os
+import os
 #os.system("""(sync;sync;sync;date;ps ax) | mail -s "Pen4 task" vasily122@yandex.ru""")
-#os.system("""(sync;date;uptime;ps ax) | mail -s "Pen4 test 2016oct11 " vasily122@yandex.ru""")
+os.system("""(sync;date;uptime;ps ax) | mail -s "Pen4 test " vasily122@yandex.ru""")
