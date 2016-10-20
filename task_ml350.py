1268660b62611ba63394a9a005833f517e3949aa
Vasily122 <vasily122@yandex.ru>
2016-10-20 15:35:50 +0100

    Update task_ml350.py

--- task_ml350.py
+++ task_ml350.py
@@ -6,8 +6,8 @@
 # Date: 2016 may 7
 # Version: 0.3
 #====================================
-#import os
+import os
 #os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc ml350" vasily122@yandex.ru""")
-#os.system("""(date;ps ax) | mail -s "ml350 task" vasily122@yandex.ru""")
+os.system("""(sync;date;uptime) | mail -s "ml350 task" vasily122@yandex.ru""")
