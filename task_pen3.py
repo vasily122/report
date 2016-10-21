662eb837fd1601487a99d832cd76f23221cfb662
Vasily122 <vasily122@yandex.ru>
2016-10-21 06:45:39 +0100

    Update task_pen3.py

--- task_pen3.py
+++ task_pen3.py
@@ -6,8 +6,9 @@
 # Date: 2016 may 7
 # Version: 0.3
 #====================================
-import os
+#import os
+#os.system("""(sync;date;uptime;ps ax) | mail -s "pen3 task" vasily122@yandex.ru""")
+
 #os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
-os.system("""(sync;date;uptime;ps ax) | mail -s "pen3 task" vasily122@yandex.ru""")
 #os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc pen3" vasily122@yandex.ru""")
