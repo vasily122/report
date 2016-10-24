2d3eb157dbeabdf15a33c080242571017c107122
Vasily122 <vasily122@yandex.ru>
2016-10-24 11:00:43 +0100

    Update task_depo.py

--- task_depo.py
+++ task_depo.py
@@ -6,8 +6,8 @@
 # Date: 2016 may 7
 # Version: 0.3
 #====================================
-import os
-os.system("""(sync;date;ps ax) | mail -s "deposerv task" vasily122@yandex.ru""")
+#import os
+#os.system("""(sync;date;ps ax) | mail -s "deposerv task" vasily122@yandex.ru""")
 
 #os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
