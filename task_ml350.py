811ac8acdc0e48b47044127d59fb8dfee88bccb0
Vasily122 <vasily122@yandex.ru>
2017-01-24 08:31:55 +0000

    Update task_ml350.py

--- task_ml350.py
+++ task_ml350.py
@@ -6,8 +6,8 @@
 # Date: 2016 may 7
 # Version: 0.3
 #====================================
-#import os
-#os.system("""(sync;date;uptime) | mail -s "HP_ml350 task" vasily122@yandex.ru""")
+import os
+os.system("""(sync;date;uptime) | mail -s "HP_ml350 task" vasily122@yandex.ru""")
 
 #os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
