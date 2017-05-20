fb7ebfd2ef1ce56a442289f95d31691a17bc8832
Vasily122 <vasily122@yandex.ru>
2017-05-20 20:04:16 +0300

    tested in wifi and smartphone as modem

--- report.py
+++ report.py
@@ -1,21 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*- 
 #====================================
-#Prog starts on @reboot in crontab
-#if network exists then load external module monitor.py
+# report.py
+# starts in crontab: @reboot /home/toma/DO/./report.py & 
+#if network exists, then load module ~/DO/./monitor.py
+# USE  Python3 !
+# launch with & !
 #Author: vasily122@yandex.ru
-#Date: 2016 july 28
-#Version: 3.1
+#Date: 2017 may 20
+#Version: 3.2
 #====================================
 import time,os
 kanal=0
 while kanal==0:
-	b=os.popen("ping -c1 8.8.8.8").read()
-	if(b[68:77] == "icmp_req=" ):
+        b=os.popen("ping -c1 8.8.8.8").read()
+        if(b[68:77] == "icmp_req=" ):
+# === TODO depends from version of ping? ===== 
 #	b=os.popen("ping -c1 8.8.8.8|grep icmp_seq= ").read()
 #	if(b[23:32] =="icmp_seq="):	
-		kanal=1
-		break
-	else:
-		time.sleep(300)
-os.system("""python /home/toma/DO/./monitor.py""")
+                kanal=1
+                os.system("""python3 ~/DO/./monitor.py""")
+                break
+        else:
+                print("sleep...")
+                time.sleep(60)
+#		time.sleep(600)
