fc484fe1167ae4ecca84c10107ce21b089e4f3ce
Vasily122 <vasily122@yandex.ru>
2017-05-20 19:57:37 +0300

    tested with wifi

--- monitor.py
+++ monitor.py
@@ -1,18 +1,18 @@
-
 #!/usr/bin/python3
 # -*- coding: utf-8 -*- 
 #====================================
 #monitor.py is for remote monitoring server\workstation
+#launch with & !
 #starts on @reboot or in time - set in crontab
-#delate previous module
+#delete previous module
 #if network exists, load new external module task_<HOSTNAME>.py,
 #makes it executible (chmod a+x) and run
 #Author: vasily122@yandex.ru
-#Date: 2016 oct 12
+#Date: 2017 may 20
 #Version: 2.2
 #====================================
 import os
-os.system("rm task_s50.py")
-os.system("wget https://raw.githubusercontent.com/vasily122/scripts/master/task_s50.py")
-os.system("chmod a+x task_s50.py")
-os.system("python task_s50.py")
+os.system("rm task_besl.py")
+os.system("wget https://raw.githubusercontent.com/vasily122/scripts/master/task_besl.py")
+os.system("chmod a+x task_besl.py")
+os.system("python task_besl.py")
