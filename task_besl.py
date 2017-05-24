7fdf58eca75ea0de560e26080f71e13c561d653c
Vasily122 <vasily122@yandex.ru>
2017-05-24 20:36:39 +0300

    Update task_besl.py

--- task_besl.py
+++ task_besl.py
@@ -3,9 +3,9 @@
 #====================================
 # Prog: external uploaded command module 
 # Author: vasily122@yandex.ru
-# Date: 2017 may 23
+# Date: 2017 may 24
 # Version: 0.4
 #====================================
 import os
-os.system(""" date >> ~/DO/log.txt """)
+os.system(""" date >> /home/toma/DO/log.txt """)
 os.system("""(date; df)| mail -s "Samsung r530" vasily122@yandex.ru """)
