623640a452e73e0c17214c98c8180a215d6207a3
Vasily122 <vasily122@yandex.ru>
2017-05-26 22:49:04 +0400

    Previous working
    Whats going on? Unstable internet usn modem?

--- task_besl.py
+++ task_besl.py
@@ -3,9 +3,9 @@
 #====================================
 # Prog: external uploaded command module 
 # Author: vasily122@yandex.ru
-# Date: 2017 may 24
+# Date: 2017 may 23
 # Version: 0.4
 #====================================
 import os
-os.system(""" date >> /home/toma/DO/log.txt """)
-os.system("""(date; df)| mail -s "Samsung r530" vasily122@yandex.ru """)
+os.system("""sync; date >> ~/DO/log.txt """)
+os.system("""(date; df; cat ~/DO/log.txt ) | mail -s " Samsung r530 boot" vasily122@yandex.ru""")
