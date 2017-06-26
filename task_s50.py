2c45f3ab4628bf110a898ccf52d84f060a2f25e7
vasily122 <vasily122@yandex.ru>
2017-06-26 12:50:17 +0300

    s50 test mail 1

--- task_s50.py
+++ task_s50.py
@@ -4,9 +4,9 @@
 #====================================
 # Prog: external uploaded command module 
 # Author: vasily122@yandex.ru
-# Date: 2016 may 7
+# Date: 2016 june 26
 # Version: 0.3
 #====================================
 #import os
-#os.system(""" (sync;date; uptime; ps ax ) | mail -s "s50 task" vasily122@yandex.ru """)
+os.system(""" (sync;date; uptime; ps ax ) | mail -s "s50 task" vasily122@yandex.ru """)
 #os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc s50" vasily122@yandex.ru""")
