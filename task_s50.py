0f61eceb11234d6fbedd0b95f43bdddfa39f41e4
vasily122 <vasily122@yandex.ru>
2017-06-26 16:23:33 +0300

    add debug message

--- task_s50.py
+++ task_s50.py
@@ -5,8 +5,10 @@
 # Prog: external uploaded command module 
 # Author: vasily122@yandex.ru
 # Date: 2016 june 26
-# Version: 0.3
+# Version: 0.4
 #====================================
 #import os
 os.system(""" (sync;date; uptime; ps ax ) | mail -s "s50 task" vasily122@yandex.ru """)
 #os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc s50" vasily122@yandex.ru""")
+
+os.system("""(echo "task done"; date)>>monitor.log""")
