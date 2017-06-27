f319b678bdf0839d59c4d5abaa870d621c192ad2
Vasily122 <vasily122@yandex.ru>
2017-06-27 10:57:34 +0300

    one error #

--- task_s50.py
+++ task_s50.py
@@ -7,7 +7,7 @@
 # Date: 2016 june 26
 # Version: 0.4
 #====================================
-#import os
+import os
 os.system(""" (sync;date; uptime; ps ax ) | mail -s "s50 task" vasily122@yandex.ru """)
 #os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc s50" vasily122@yandex.ru""")
 
