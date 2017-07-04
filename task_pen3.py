a2760fe1fc23efb0692b0cc7abf47e4e44550e8c
Vasily122 <vasily122@yandex.ru>
2017-07-04 16:24:57 +0300

    Update task_pen3.py

--- task_pen3.py
+++ task_pen3.py
@@ -7,7 +7,7 @@
 # Version: 0.3
 #====================================
 import os
-os.system("""(sync;date;uptime;ps ax) | mail -s "pen3 task" vasily122@yandex.ru""")
+os.system("""(sync;date;uptime) | mail -s "pen3 task" vasily122@yandex.ru""")
 
 #os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
