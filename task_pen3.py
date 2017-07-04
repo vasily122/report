d05523e654f271752259cd8bb498678d118fe0b8
Vasily122 <vasily122@yandex.ru>
2017-07-04 16:29:55 +0300

    old code cleaned

--- task_pen3.py
+++ task_pen3.py
@@ -3,12 +3,8 @@
 #====================================
 # Prog: external uploaded command module 
 # Author: vasily122@yandex.ru
-# Date: 2016 may 7
-# Version: 0.3
+# Date: 2017 july 04
+# Version: 0.3.1
 #====================================
 import os
 os.system("""(sync;date;uptime) | mail -s "pen3 task" vasily122@yandex.ru""")
-
-#os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
-#os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
-#os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc pen3" vasily122@yandex.ru""")
