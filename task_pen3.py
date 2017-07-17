5026770e488f7019991e7be77969d77a4360f266
Vasily122 <vasily122@yandex.ru>
2017-07-17 11:54:24 +0300

    Update task_pen3.py

--- task_pen3.py
+++ task_pen3.py
@@ -3,10 +3,10 @@
 #====================================
 # Prog: external uploaded command module 
 # Author: vasily122@yandex.ru
-# Date: 2017 july 04
+# Date: 2017 july 17
 # Version: 0.3.1
 #====================================
 import os
 #os.system("""(sync;date;uptime;cat /sys/class/thermal/thermal_zone*/temp) | mail -s "Pen III thermal test" vasily122@yandex.ru""")
 #os.system("""(date;ls /sys/class/) | mail -s "=== III  vasily122@yandex.ru""")
-os.system("""(sync;date;uptime;ls /sys) | mail -s "Pen III thermal test" vasily122@yandex.ru""")
+os.system("""(sync;date;uptime;ls /sys/class/) | mail -s "Pen III thermal test" vasily122@yandex.ru""")
