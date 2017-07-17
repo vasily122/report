eab5636470e3b1e7017bdfb70de22009bf23857d
Vasily122 <vasily122@yandex.ru>
2017-07-17 15:34:18 +0300

    nop

--- task_pen3.py
+++ task_pen3.py
@@ -6,7 +6,7 @@
 # Date: 2017 july 17
 # Version: 0.3.1
 #====================================
-import os
+#import os
 #os.system("""(sync;date;uptime;cat /sys/class/thermal/thermal_zone*/temp) | mail -s "Pen III thermal test" vasily122@yandex.ru""")
 #os.system("""(date;ls /sys/class/) | mail -s "=== III  vasily122@yandex.ru""")
-os.system("""(sync;date;uptime;ls -R /sys/class/;uptime;ls -R -la /sys/class/ ) | mail -s "Pen III thermal test1" vasily122@yandex.ru""")
+#os.system("""(sync;date;uptime;ls -R /sys/class/;uptime;ls -R -la /sys/class/ ) | mail -s "Pen III thermal test1" vasily122@yandex.ru""")
