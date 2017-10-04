baee084fb6313b6a7c00ec0c5b5f69470666e67f
Vasily122 <vasily122@yandex.ru>
2017-10-04 12:47:17 +0300

    new instructions, sleeping time, log

--- report.py
+++ report.py
@@ -3,30 +3,28 @@
 #====================================
 # report.py
 # starts in crontab: @reboot /home/toma/DO/./report.py &
-#if network exists, then load module ~/DO/./monitor.py
-# USE  Python3 !
-# launch with & !
-#Author: vasily122@yandex.ru
-#Date: 2017 june 27
-#Version: 3.3
+# if network exists, then load module ~/DO/./monitor.py
+# USE  Python3 
+# launch with & 
+# Author: vasily122@yandex.ru
+# Date: 2017 oct 04
+# Version: 3.4
 #====================================
 import time,os
 kanal=0
 while kanal==0:
 # === depends from version of ping => for ubuntu 12.04 LTS
+# === Also works with cable ethrnet
 #        b=os.popen("ping -c1 8.8.8.8|grep icmp_seq= ").read()
 #        if(b[68:77] == "icmp_req=" ):
 
-# === depends from version of ping => iputils-s20161105  on ubuntu 17.04
+# === depends from version of ping => iputils-s20161105 on ubuntu 16.04,17.04
+# === works with Megafon
         b=os.popen("ping -c1 8.8.8.8|grep icmp_seq= ").read()
         if(b[23:32] =="icmp_seq="):
                 kanal=1
                 os.system("""python3 ~/DO/./monitor.py""")
-#                print("""network present""")
                 break
         else:
-#                print("sleep...")
-                time.sleep(60)
-
-#print("""report.py done""")
+                time.sleep(100)
 os.system("""(echo "report done"; date)>>monitor.log""")
