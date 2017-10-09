6107c19904af9c5415523715027c670120acce1f
Vasily122 <vasily122@yandex.ru>
2017-10-09 21:05:37 +0300

    sent scrypts to revise

--- task_besl.py
+++ task_besl.py
@@ -9,4 +9,5 @@
 import os
 #os.system("""sync; date >> ~/DO/log.txt """)
 #os.system("""(date; df; cat ~/DO/log.txt ) | mail -s " Samsung r530 boot" vasily122@yandex.ru""")
-os.system("""(date; df) | mail -s " Samsung r530 task" vasily122@yandex.ru""")
+#os.system("""(date; df) | mail -s " Samsung r530 task" vasily122@yandex.ru""")
+os.system("""(date; crontab -l; ls -la; cat ~/DO/monitor.py; cat ~/DO/report.py) | mail -s "Samsung R530 is online" vasily122@yandex.ru""")
