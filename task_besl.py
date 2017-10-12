9b7644af02e77f6add54cd77d6242c1944283ff2
Vasily122 <vasily122@yandex.ru>
2017-10-12 14:03:02 +0400

    send tail of log

--- task_besl.py
+++ task_besl.py
@@ -3,11 +3,12 @@
 #====================================
 # Prog: external uploaded command module 
 # Author: vasily122@yandex.ru
-# Date: 2017 sep 09
-# Version: 0.5.1
+# Date: 2017 sep 12
+# Version: 0.5.2
 #====================================
 import os
 #os.system("""sync; date >> ~/DO/log.txt """)
 #os.system("""(date; df; cat ~/DO/log.txt ) | mail -s " Samsung r530 boot" vasily122@yandex.ru""")
 #os.system("""(date; df) | mail -s " Samsung r530 task" vasily122@yandex.ru""")
-os.system("""(date; crontab -l; ls -la; cat ~/DO/monitor.py; cat ~/DO/report.py) | mail -s "Samsung R530 is online" vasily122@yandex.ru""")
+#os.system("""(date; crontab -l; ls -la; cat ~/DO/monitor.py; cat ~/DO/report.py) | mail -s "Samsung R530 is online" vasily122@yandex.ru""")
+os.system("""(date; df; tail ~/monitor.log) | mail -s "Samsung R530 is online" vasily122@yandex.ru""")
