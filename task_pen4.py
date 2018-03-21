482c94c4c4b560acbe379d433de999af4d861141
Vasily122 <vasily122@yandex.ru>
2018-03-21 15:13:18 +0300

    nop 2018 mar21

--- task_pen4.py
+++ task_pen4.py
@@ -6,5 +6,5 @@
 # Date: 2018 mar 21
 # Version: 0.3.1
 #====================================
-import os
-os.system("""(sync;date;uptime;ps ax) | mail -s "Pen4 alive test 1 " vasily122@yandex.ru""")
+#import os
+#os.system("""(sync;date;uptime;ps ax) | mail -s "Pen4 alive test 1 " vasily122@yandex.ru""")
