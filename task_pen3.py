c82816396a048146a160b32cbcbbba58cccc5acd
Vasily122 <vasily122@yandex.ru>
2018-03-21 14:33:04 +0300

    test2

--- task_pen3.py
+++ task_pen3.py
@@ -4,10 +4,10 @@
 # Prog: external uploaded command module 
 # Author: vasily122@yandex.ru
 # Date: 2018 mar 21
-# Version: 0.3.2
+# Version: 0.3.3
 #====================================
 import os
-os.system("""(sync;date;uptime;ps ax ) | mail -s "Pen III alive test 1" vasily122@yandex.ru""")
+os.system("""(sync;date;uptime;ps ax) | mail -s "Pen 3 alive test 2 " vasily122@yandex.ru""")
 #os.system("""(sync;date;uptime;cat /sys/class/thermal/thermal_zone*/temp) | mail -s "Pen III thermal test" vasily122@yandex.ru""")
 #os.system("""(date;ls /sys/class/) | mail -s "=== III  vasily122@yandex.ru""")
 #os.system("""(sync;date;uptime;ls -R /sys/class/;uptime;ls -R -la /sys/class/ ) | mail -s "Pen III thermal test1" vasily122@yandex.ru""")
