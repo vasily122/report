002af00284b2499cec47d895cf00b42fbe532ca4
Vasily122 <vasily122@yandex.ru>
2018-03-21 15:17:14 +0300

    nop 2018 mar 21
    host is gone forewer

--- task_nvid.py
+++ task_nvid.py
@@ -12,4 +12,3 @@
 #os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc nvid" vasily122@yandex.ru""")
-
