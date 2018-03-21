b94ddaa5efb3c208154d9d30069e00dc3c424bd1
Vasily122 <vasily122@yandex.ru>
2018-03-21 15:11:37 +0300

    nop 2018mar21

--- task_depo.py
+++ task_depo.py
@@ -6,11 +6,11 @@
 # Date: 2018 mar 21
 # Version: 0.3.1
 #====================================
-import os
+#import os
 #os.system("""(cat /etc/init.d/boinc-client; cat /etc/default/boinc-client) | mail -s "depo task2" vasily122@yandex.ru""")
 
 
-os.system("""(sync;uptime;date;ps ax) | mail -s "deposerv alive test 1" vasily122@yandex.ru""")
+#os.system("""(sync;uptime;date;ps ax) | mail -s "deposerv alive test 1" vasily122@yandex.ru""")
 
 #os.system("""(sync;uptime;date;ps ax) | mail -s "deposerv task" vasily122@yandex.ru""")system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
 #os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
