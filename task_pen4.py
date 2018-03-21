a5737616b78801691bfaa001d5f750b642b86a7f
Vasily122 <vasily122@yandex.ru>
2018-03-21 12:26:11 +0300

    alive test 1.1 2018 mar 21

--- task_pen4.py
+++ task_pen4.py
@@ -4,7 +4,7 @@
 # Prog: external uploaded command module 
 # Author: vasily122@yandex.ru
 # Date: 2018 mar 21
-# Version: 0.3
+# Version: 0.3.1
 #====================================
 import os
-os.system("""(sync;date;uptime;ps ax) | mail -s "Pen4 task " vasily122@yandex.ru""")
+os.system("""(sync;date;uptime;ps ax) | mail -s "Pen4 alive test 1 " vasily122@yandex.ru""")
