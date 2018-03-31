5a2ee94b38ce3fac2d8bd220d25bff4f6bb3784b
Vasily122 <vasily122@yandex.ru>
2018-03-31 19:23:15 +0300

    why not running boinc?

--- task_ml350.py
+++ task_ml350.py
@@ -6,12 +6,12 @@
 # Date: 2018 mar 21
 # Version: 0.3.11
 #====================================
-#import os
+import os
 #os.system("""(sync;date;uptime;cat /sys/class/thermal/thermal_zone*/temp ) | mail -s "HP 350 thermal test2" vasily122@yandex.ru""")
 
 #os.system("""(sync;date;uptime; ps ax) | mail -s "HP_ml350 alive test 1" vasily122@yandex.ru""")
 
-#os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
-#os.system("""(date;sudo dmidecode 2>&1) | mail -s "s5000 task" vasily122@yandex.ru""")
-#os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc ml350" vasily122@yandex.ru""")
+#os.system("""(date;uptime;lshw;inxi -AbD 2>&1) | mail -s "HP_ml350 task" vasily122@yandex.ru""")
+#os.system("""(date;sudo dmidecode 2>&1) | mail -s "HP_ml350 task" vasily122@yandex.ru""")
+os.system("""(date ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc HP_ml350" vasily122@yandex.ru""")
 
