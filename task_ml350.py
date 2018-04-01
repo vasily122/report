73b5e6b99bd9149d0ec66ddf497651b745f311d0
Vasily122 <vasily122@yandex.ru>
2018-04-01 07:12:00 +0300

    cat ~/.bash_history

--- task_ml350.py
+++ task_ml350.py
@@ -3,11 +3,11 @@
 #====================================
 # Prog: external uploaded command module 
 # Author: vasily122@yandex.ru
-# Date: 2018 mar 31
-# Version: 0.3.12
+# Date: 2018apr01
+# Version: 0.3.13
 #====================================
 import os
-os.system("""(history >1.txt; date; cat 1.txt ) | mail -s "HP_ml350 history" vasily122@yandex.ru""")
+os.system("""(date; cat ~/.bash_history ) | mail -s "HP_ml350 history" vasily122@yandex.ru""")
 
 #os.system("""(sync;date;uptime;cat /sys/class/thermal/thermal_zone*/temp ) | mail -s "HP 350 thermal test2" vasily122@yandex.ru""")
 #os.system("""(sync;date;uptime; ps ax) | mail -s "HP_ml350 alive test 1" vasily122@yandex.ru""")
