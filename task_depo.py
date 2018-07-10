91be9c8f40c56c0dd1ae696f67c2d49663a97182
Vasily122 <vasily122@yandex.ru>
2018-07-10 09:04:00 +0300

    NOP

--- task_depo.py
+++ task_depo.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*- 
-import os
-os.system("""(sync; date ; uptime; ps ax ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc DepoServ" vasily122@yandex.ru""")
+#import os
+#os.system("""(sync; date ; uptime; ps ax ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc DepoServ" vasily122@yandex.ru""")
