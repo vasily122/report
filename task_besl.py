4ba631f01d430a26fcb9c4f0be77af17dfcaa6d9
Vasily122 <vasily122@yandex.ru>
2018-07-20 18:54:36 +0300

    sync

--- task_besl.py
+++ task_besl.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*- 
 import os
-os.system("""(date; df; tail ~/monitor.log) | mail -s "Samsung_R530 is online" vasily122@yandex.ru""")
+os.system("""(date; df; tail ~/monitor.log; sync) | mail -s "Samsung_R530 is online" vasily122@yandex.ru""")
