ad0043230f777b06809804b1ea912a3cfffbe9b7
Vasily122 <vasily122@yandex.ru>
2018-08-27 17:16:15 +0300

    test 1 2018aug27

--- task_pen3.py
+++ task_pen3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
-#import os
-#os.system("""(sync;date;uptime;ps ax) | mail -s "Pen 3 alive test 2 " vasily122@yandex.ru""")
+import os
+os.system("""(sync;date;uptime;ps ax) | mail -s "Pen 3 alive " vasily122@yandex.ru""")
