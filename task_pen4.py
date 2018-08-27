ce84e91ed7ec45d429bec8f38c90c09d8bfc443b
Vasily122 <vasily122@yandex.ru>
2018-08-27 17:46:43 +0300

    NOP

--- task_pen4.py
+++ task_pen4.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
-import os
-os.system("""(sync;date;uptime;ps ax) | mail -s "Pen4 alive " vasily122@yandex.ru""")
+#import os
+#os.system("""(sync;date;uptime;ps ax) | mail -s "Pen4 alive " vasily122@yandex.ru""")
