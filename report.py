0147600a9f2578179541572b6ec5dfcbbb23b954
Vasily122 <vasily122@yandex.ru>
2018-09-29 23:59:22 +0300

    V3.6
    add comments and usage

--- report.py
+++ report.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #====================================
-# report.py
-# starts in crontab: @reboot /home/toma/DO/./report.py &
-# if network exists, then starts module ~/DO/./monitor.py
-# USE  Python3 
-# launch with & in background!
+#report.py - core of remote monitoring
+#USAGE:
+#      crontab -e
+#      @reboot /home/toma/DO/./report.py &
+# NOTE: install Python3,launch with & in background!
 # Author: vasily122@yandex.ru
-# Date: 2018 june 09 (revised)
-# Version: 3.5
+# Date: 2018 september 29 (revised)
+# Version: 3.6
 #====================================
 import time,os
 kanal=0
-#lets wait 5 min before 1 test...
+#before mobile network starts, lets wait 5 min before 1 test...
 time.sleep(300)
 while kanal==0:
+# if network exists, then starts module ~/DO/./monitor.py
         b=os.popen("ping -c1 8.8.8.8|grep icmp_seq= ").read()
         if(b[23:32] =="icmp_seq="):
-                kanal=1
+                kanal=1 #network exists
                 os.system("""python3 ~/DO/./monitor.py""")
                 break
         else:
