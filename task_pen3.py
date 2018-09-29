b38cb92d16e5a0ee99f4ed295c869831aa561de2
Vasily122 <vasily122@yandex.ru>
2018-09-29 23:58:12 +0300

    pen3 probes pen4

--- task_pen3.py
+++ task_pen3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*- 
 import os
-os.system("""(sync;date;uptime;ps ax) | mail -s "Pen3 responce " vasily122@yandex.ru""")
+#os.system("""(sync;date;uptime;ps ax) | mail -s "Pen3 responce " vasily122@yandex.ru""")
+os.system("""(date;ping -c1 192.168.10.91;ping -c1 192.168.10.92)| mail -s "pen3 probe" vasily122@yandex.ru""")
