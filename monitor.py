d3f5783e73740a6affbf3c7bca3c498e7095e34f
Vasily122 <vasily122@yandex.ru>
2018-09-29 23:36:33 +0300

    V3.3
    changed repesitory name
    make usage and code clear

--- monitor.py
+++ monitor.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*- 
 #====================================
-#monitor.py is for remote monitoring server\workstation
-#launch with & in background!
-#starts on @reboot or in time - set in crontab
-#delete previous module
-#if network exists, load new external module task_<HOSTNAME>.py,
-#makes it executible (chmod a+x) and run
+#monitor.py -remote monitoring linux server\workstation script in python3
+#USAGE:
+#      crontab -e
+#      0 * * * * python3 /home/...path.../monitor.py
 #Author: vasily122@yandex.ru
-#Date: 2018 june 09(revised)
-#Version: 3.2
+#Date: 2018 september 29
+#Version: 3.3
 #====================================
 import os
-filename="task_s50.py"
+#set filename for proper server: task_<HOSTNAME>.py
+filename="task_tower.py"
+#at the beginning delete previous module
 os.system("rm "+filename)
-os.system("wget https://raw.githubusercontent.com/vasily122/scripts/master/"+filename)
+#load new external module  (network must exist)
+os.system("wget https://raw.githubusercontent.com/vasily122/report/master/"+filename)
+#make it executible and run
 os.system("chmod a+x "+filename)
 os.system("python3 "+filename)
 os.system("""(echo "monitor done"; date) >> monitor.log""")
