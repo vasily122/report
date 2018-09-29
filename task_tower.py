0e511110022354a143dfc0324adade8d1c8446a7
Vasily122 <vasily122@yandex.ru>
2018-09-29 19:38:53 +0300

    responce2018sep29

--- task_tower.py
+++ task_tower.py
@@ -3,4 +3,4 @@
 # -*- coding: utf-8 -*- 
 import os
 #os.system("""(sync; date ; uptime; ps ax ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc DepoServ" vasily122@yandex.ru""")
-os.system("""(sync; date ; uptime; ps ax)| mail -s "server responce" vasily122@yandex.ru""")
+os.system("""(sync; date ; uptime; ps ax)| mail -s "tower responce" vasily122@yandex.ru""")
