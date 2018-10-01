cae6911d4f7a5fdd391ad93a7131d8c2116008b0
Vasily122 <vasily122@yandex.ru>
2018-10-01 15:09:00 +0300

    python2

--- task_pen4.py
+++ task_pen4.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 import os
-os.system("""(sync;date;uptime;ps ax) | mail -s "Pen4 responce" vasily122@yandex.ru""")
+os.system("""(sync;date;uptime;ps ax) | mail -s "Pen4" vasily122@yandex.ru""")
