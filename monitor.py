521d25ca3636fb5448ea0210a79db08c56fb606d
Vasily122 <vasily122@yandex.ru>
2018-10-01 20:27:43 +0300

    V3.7
    change to python 2 
    changed name of repository

--- monitor.py
+++ monitor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #====================================
 #monitor.py -remote monitoring linux server\workstation script in python3
@@ -6,11 +6,11 @@
 #      crontab -e
 #      0 * * * * python3 /home/...path.../monitor.py
 #Author: vasily122@yandex.ru
-#Date: 2018 september 29
-#Version: 3.6
+#Date: 2018 october 01
+#Version: 3.7
 #====================================
 import os
-#set filename for proper server: task_<HOSTNAME>.py
+#set filename for proper server: task_<HOSTNAME>.py( from REPOSITORY "report" !!!)
 filename="task_tower.py"
 #at the beginning delete previous module
 os.system("rm "+filename)
@@ -18,5 +18,5 @@ os.system("rm "+filename)
 os.system("wget https://raw.githubusercontent.com/vasily122/report/master/"+filename)
 #make it executible and run
 os.system("chmod a+x "+filename)
-os.system("python3 "+filename)
-os.system("""(echo "monitor done"; date) >> monitor.log""")
+os.system("python "+filename)
+os.system("""(echo "monitoring done"; date) >> monitor.log""")
