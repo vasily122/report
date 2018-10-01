6bbaf3f8dc0bb11551af9511496ae1879cf9b059
Vasily122 <vasily122@yandex.ru>
2018-10-01 20:24:25 +0300

    aqua get monitor
    aqua get monitor

--- task_aqua.py
+++ task_aqua.py
@@ -3,3 +3,5 @@
 import os
 os.system("""(sync; date ; uptime; ps ax ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc aqua" vasily122@yandex.ru""")
 #os.system("""(sync; date ; uptime)| mail -s "Depo" vasily122@yandex.ru""")
+os.system("""wget https://raw.githubusercontent.com/vasily122/report/master/monitor.py""")
+os.system("""ls -la|mail -s "aqua get monitor" vasily122@yandex.ru""")
