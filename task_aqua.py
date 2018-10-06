d4e92cf8b063dbe15535cc05207811bebfa0d605
Vasily122 <vasily122@yandex.ru>
2018-10-06 18:03:07 +0300

    NOP

--- task_aqua.py
+++ task_aqua.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
-import os
+#import os
 #os.system("""(sync; date ; uptime; ps ax ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc aqua" vasily122@yandex.ru""")
-os.system("""(sync; date ; uptime)| mail -s "aquarius" vasily122@yandex.ru""")
+#os.system("""(sync; date ; uptime;ps ax)| mail -s "Aquarius" vasily122@yandex.ru""")
 #os.system("""wget https://raw.githubusercontent.com/vasily122/report/master/monitor.py""")
 #os.system("""ls -la|mail -s "aqua get monitor" vasily122@yandex.ru""")
