4d2bb3d5efc82a90c6a003cb5495b45341e2baeb
Vasily122 <vasily122@yandex.ru>
2018-10-06 18:10:17 +0300

    V3.7
    Everyday production version

--- task_besl.py
+++ task_besl.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 import os
-os.system("""(sync; date; df) | mail -s "Samsung_R530 is online" vasily122@yandex.ru""")
+os.system("""(sync; date; df; tail ~/monitor.log) | mail -s "Samsung_R530 is online" vasily122@yandex.ru""")
