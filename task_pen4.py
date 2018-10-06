68ebd30aaf749876328b7233ee9dd9620a7f921c
Vasily122 <vasily122@yandex.ru>
2018-10-06 13:58:11 +0300

    Update task_pen4.py

--- task_pen4.py
+++ task_pen4.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
-#import os
-#os.system("""(sync;date;uptime;ps ax) | mail -s "Pen4" vasily122@yandex.ru""")
+import os
+os.system("""(sync;date;uptime;ps ax) | mail -s "Pen4" vasily122@yandex.ru""")
 #os.system("""wget https://raw.githubusercontent.com/vasily122/report/master/monitor.py""")
 #os.system("""ls -la|mail -s "pen4 get monitor" vasily122@yandex.ru""")
