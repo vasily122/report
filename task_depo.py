c1e859a47e9942ece91330e4134dd26e0a592873
Vasily122 <vasily122@yandex.ru>
2019-01-03 23:01:02 +0300

    Update task_depo.py

--- task_depo.py
+++ task_depo.py
@@ -6,4 +6,4 @@ import os
 #os.system("""(date;ping -c3 192.168.10.90;ping -c3 192.168.10.94)| mail -s "Depo responce" vasily122@yandex.ru""")
 #os.system("""wget https://raw.githubusercontent.com/vasily122/report/master/monitor.py""")
 #os.system("""ls -la|mail -s "depo get monitor" vasily122@yandex.ru""")
-os.system("""(date;python -V;python3 -V)|mail -s "depo python versions" vasily122@yandex.ru""")
+os.system("""(date;python -V;uptime)|mail -s "depo python versions" vasily122@yandex.ru""")
