0f7c829c5dc2502642f6ab9c90e4e875f7f4045f
Vasily122 <vasily122@yandex.ru>
2019-01-04 13:42:17 +0300

    NOP

--- task_pen3.py
+++ task_pen3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
-import os
+#import os
 #os.system("""(sync; date; uptime; ps ax; crontab -l; cat ~/DO/monitor.py; cat ~/task_pen3.py) | mail -s "Pen3 " vasily122@yandex.ru""")
 #os.system("""(date;crontab -l;cat ~/DO/monitor.py)| mail -s "pen3" vasily122@yandex.ru""")
 #os.system("""wget https://raw.githubusercontent.com/vasily122/report/master/monitor.py""")
 #os.system("""ls -la|mail -s "pen3 get monitor" vasily122@yandex.ru""")
-os.system("""(date;python3 --version;uptime)|mail -s "pen3 python versions" vasily122@yandex.ru""")
+#os.system("""(date;python3 --version;uptime)|mail -s "pen3 python versions" vasily122@yandex.ru""")
