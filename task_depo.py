654cdef945a47d0f8407011b35604d193c257884
Vasily122 <vasily122@yandex.ru>
2019-03-01 11:14:16 +0300

    test after power failure

--- task_depo.py
+++ task_depo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
-#import os
+import os
+os.system("""(sync; date ; uptime; ps ax )| mail -s "boinc DepoServ" vasily122@yandex.ru""")
 #os.system("""(sync; date ; uptime; ps ax ; cat /var/lib/boinc-client/stderrdae.txt; cat /var/lib/boinc-client/stdoutdae.txt; cat /var/lib/boinc-client/time_stats_log)| mail -s "boinc DepoServ" vasily122@yandex.ru""")
 #os.system("""(sync; date; uptime; ps ax; crontab -l; cat ~/DO/monitor.py; cat ~/task_depo.py)| mail -s "Depo" vasily122@yandex.ru""")
 #os.system("""(date;ping -c3 192.168.10.90;ping -c3 192.168.10.94)| mail -s "Depo responce" vasily122@yandex.ru""")
