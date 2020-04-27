73ecefbece62b468ede7b7ecdd3bd166290e00f0
Vasily122 <vasily122@yandex.ru>
2020-04-27 17:25:43 +0300

    test 0.14

--- task_pen3.py
+++ task_pen3.py
@@ -5,8 +5,9 @@ import os
 
 os.system("""rm index.html""")
 os.system("""wget opennet.ru""")
-os.system("""(date;ls) | mail -s "Pen3 test 0.13" vasily122@yandex.ru""")
-os.system("""mail -s "Pen3 test 0.13 attach" vasily122@yandex.ru < index.html""")
+#os.system("""(date;ls) | mail -s "Pen3 test 0.13" vasily122@yandex.ru""")
+os.system("""mail -s "Pen3 test 0.14 attach" vasily122@yandex.ru < index.html""")
+os.system("""mail -s "Pen3 test 0.14 attach" -a index.html vasily122@yandex.ru""")
 #os.system("""(cat index.html) | mail -s "Pentium3 news digest" vasily122@yandex.ru""")
 #os.system("""rm index.html""")
 
