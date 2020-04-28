18070d45e28bbb7d73d8004cfa000f6e110562ab
Vasily122 <vasily122@yandex.ru>
2020-04-28 09:42:04 +0300

    NOP

--- task_pen3.py
+++ task_pen3.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 # 2020apr27
-import os
 
-os.system("""rm index.html""")
-os.system("""lynx --dump opennet.ru""")
-os.system("""mail -s "Pen3 test 0.16 linx" vasily122@yandex.ru < index.html""")
+#import os
+##os.system("""rm index.html""")
+##os.system("""lynx --dump opennet.ru""")
+##os.system("""mail -s "Pen3 test 0.16 linx" vasily122@yandex.ru < index.html""")
 
-os.system("""rm index.html""")
-os.system("""wget opennet.ru""")
-os.system("""(cat index.html)|mail -s "Pen3 test 0.16 wget" vasily122@yandex.ru""")
+#os.system("""rm index.html""")
+#os.system("""wget opennet.ru""")
+#os.system("""(cat index.html)|mail -s "Pen3 test 0.16 wget" vasily122@yandex.ru""")
 
 #os.system("""(date;ls) | mail -s "Pen3 test 0.13" vasily122@yandex.ru""")
-
 #os.system("""(cat index.html) | mail -s "Pentium3 news digest" vasily122@yandex.ru""")
 #os.system("""rm index.html""")
-
 #os.system("""(sync; date; uptime; ps ax) | mail -s "PentiumIII" vasily122@yandex.ru""")
 #os.system("""(sync; date; uptime; ps ax; crontab -l; cat ~/DO/monitor.py; cat ~/task_pen3.py) | mail -s "Pen3 " vasily122@yandex.ru""")
 #os.system("""(date;crontab -l;cat ~/DO/monitor.py)| mail -s "pen3" vasily122@yandex.ru""")
