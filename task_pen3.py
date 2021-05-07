47756c52e5333fa5ece4d07d57d9f42ab8d45d6c
Vasily122 <vasily122@yandex.ru>
2021-05-07 14:59:32 +0300

    Update task_pen3.py

--- task_pen3.py
+++ task_pen3.py
@@ -7,8 +7,8 @@ import os
 
 
 os.system("""rm news.txt)""")
-os.system("""links -dump http://wttr.in/Vladikavkaz > news.txt""")
-
+os.system("""wget -O news.txt https://services.swpc.noaa.gov/text/3-day-geomag-forecast.txt""")
+os.system("""links -dump http://wttr.in/Vladikavkaz >> news.txt""")
 os.system("""(sync; date; ps ax; uptime;cat news.txt)| mail -s "pen3 milkyway" vasily122@yandex.ru""")
 
 #os.system("""links -dump https://www.opennet.ru/opennews/ > news.txt""")
