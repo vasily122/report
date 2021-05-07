3e89395d07c3419238da1a57a7df0ebcd9d954ce
Vasily122 <vasily122@yandex.ru>
2021-05-07 23:05:43 +0300

    digest 0.8 working,NOP

--- task_pen3.py
+++ task_pen3.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*- 
 #2021feb01
 #2021apr26 - 2021may04 - processor fan died, repaired by Maks
-import os
-os.system("""wget -O news.txt ftp://ftp.swpc.noaa.gov/pub/latest/dayind.txt""")
-os.system("""links -dump http://wttr.in/Vladikavkaz?Fq1n >> news.txt""")
-os.system("""(sync; date; uptime; cat news.txt)| mail -s "pen3 DIGEST 0.7" vasily122@yandex.ru""")
-#os.system("""(sync; date; ps ax; uptime)| mail -s "pen3 0.5" vasily122@yandex.ru""")
 
+##import os
+##os.system("""wget -O news.txt ftp://ftp.swpc.noaa.gov/pub/latest/dayind.txt""")
+##os.system("""links -dump http://wttr.in/Vladikavkaz?Fq1n >> news.txt""")
+##os.system("""(sync; date; uptime; cat news.txt)| mail -s "pen3 DIGEST 0.7" vasily122@yandex.ru""")
+
+#os.system("""(sync; date; ps ax; uptime)| mail -s "pen3 0.5" vasily122@yandex.ru""")
 #os.system("""links -dump http://wttr.in/Vladikavkaz?Fq2 >> news.txt""")
 ##os.system("""rm news.txt)""")
 #os.system("""links -dump https://www.opennet.ru/opennews/ > news.txt""")
@@ -16,9 +17,7 @@ os.system("""(sync; date; uptime; cat news.txt)| mail -s "pen3 DIGEST 0.7" vasil
 #os.system("""wget -O index.html opennet.ru/index.shtml""")
 ##os.system("""(date;cat w.txt; uptime; cat news.txt) | mail -s "Pentium3 news digest 0.4" vasily122@yandex.ru""")
 #os.system("""(date; ls -R /var/lib/boinc-client; cat /var/lib/boinc-client/gui_rpc_auth.cfg)| mail -s "pen3 boinc" vasily122@yandex.ru""")
-
 #os.system("""(sync; crontab -l; cat ~/DO/monitor.py; ls ~/DO/; cat /etc/issue; )| mail -s "Pen3 server files" vasily122@yandex.ru""")
-
 #os.system("""(sync; uptime; ps ax; cat /var/lib/boinc-client/stderrdae.txt; date; cat /var/lib/boinc-client/stdoutdae.txt; date; cat /var/lib/boinc-client/time_stats_log)| mail -s "pen3 milky errors" vasily122@yandex.ru""")
 #os.system("""(date; cat .bash_history)| mail -s "Pen3 history" vasily122@yandex.ru""")
 #os.system("""(date;ls) |mail -s "Pen3 html test 0.1" vasily122@yandex.ru""")
