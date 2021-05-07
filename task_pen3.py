426f63a300797a0048a7fd27157d877acd7d9521
Vasily122 <vasily122@yandex.ru>
2021-05-07 16:07:31 +0300

    hour digest 0.7

--- task_pen3.py
+++ task_pen3.py
@@ -6,8 +6,9 @@ import os
 #os.system("""(sync; date; ps ax; uptime)| mail -s "pen3 0.5" vasily122@yandex.ru""")
 ##os.system("""rm news.txt)""")
 os.system("""wget -O news.txt ftp://ftp.swpc.noaa.gov/pub/latest/dayind.txt""")
-os.system("""links -dump http://wttr.in/Vladikavkaz?Fq2 >> news.txt""")
-os.system("""(sync; date; uptime;ls -la; cat news.txt)| mail -s "pen3 DIGEST 0.6" vasily122@yandex.ru""")
+#os.system("""links -dump http://wttr.in/Vladikavkaz?Fq2 >> news.txt""")
+os.system("""links -dump http://wttr.in/Vladikavkaz?Fq1n >> news.txt""")
+os.system("""(sync; date; uptime; cat news.txt)| mail -s "pen3 DIGEST 0.6" vasily122@yandex.ru""")
 
 #os.system("""links -dump https://www.opennet.ru/opennews/ > news.txt""")
 #os.system("""links -dump opennet.ru >> news.txt""")
@@ -16,7 +17,7 @@ os.system("""(sync; date; uptime;ls -la; cat news.txt)| mail -s "pen3 DIGEST 0.6
 ##os.system("""(date;cat w.txt; uptime; cat news.txt) | mail -s "Pentium3 news digest 0.4" vasily122@yandex.ru""")
 #os.system("""(date; ls -R /var/lib/boinc-client; cat /var/lib/boinc-client/gui_rpc_auth.cfg)| mail -s "pen3 boinc" vasily122@yandex.ru""")
 
-os.system("""(sync; crontab -l; cat ~/DO/monitor.py; ls ~/DO/; cat /etc/issue; )| mail -s "Pen3 server files" vasily122@yandex.ru""")
+#os.system("""(sync; crontab -l; cat ~/DO/monitor.py; ls ~/DO/; cat /etc/issue; )| mail -s "Pen3 server files" vasily122@yandex.ru""")
 
 #os.system("""(sync; uptime; ps ax; cat /var/lib/boinc-client/stderrdae.txt; date; cat /var/lib/boinc-client/stdoutdae.txt; date; cat /var/lib/boinc-client/time_stats_log)| mail -s "pen3 milky errors" vasily122@yandex.ru""")
 #os.system("""(date; cat .bash_history)| mail -s "Pen3 history" vasily122@yandex.ru""")
